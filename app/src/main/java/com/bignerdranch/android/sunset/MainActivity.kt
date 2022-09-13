0198266141cfa2f9eee60b73883ac1f82f43c869
LukaszTheLittle <lukis.maly@gmail.com>
2022-09-13 17:42:59 +0200

    Pulled out sunset colors and added animating sky colors

--- app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AccelerateInterpolator
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sceneView: View
     private lateinit var sunView: View
     private lateinit var skyView: View
+
+    private val blueSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.blue_sky)
+    }
+    private val sunsetSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.sunset_sky)
+    }
+    private val nightSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.night_sky)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +46,11 @@ class MainActivity : AppCompatActivity() {
 
         heightAnimator.interpolator = AccelerateInterpolator()
 
+        val sunsetSkyAnimator = ObjectAnimator
+            .ofInt(skyView, "backgroundColor", blueSkyColor, sunsetSkyColor)
+            .setDuration(3000)
+
         heightAnimator.start()
+        sunsetSkyAnimator.start()
     }
 }
