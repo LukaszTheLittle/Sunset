0de5e5021a3e6b33b3f71416ce5321369227e8be
LukaszTheLittle <lukis.maly@gmail.com>
2022-09-13 17:51:40 +0200

    Provided `ArgbEvaluator`

--- app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.sunset
 
+import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -49,6 +50,8 @@ class MainActivity : AppCompatActivity() {
         val sunsetSkyAnimator = ObjectAnimator
             .ofInt(skyView, "backgroundColor", blueSkyColor, sunsetSkyColor)
             .setDuration(3000)
+
+        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
 
         heightAnimator.start()
         sunsetSkyAnimator.start()
